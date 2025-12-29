6b41cec6574bee81cfce6b66cbc69db759922aad
razaamohsin196-sys <razaamohsin196@gmail.com>
2025-12-29 18:25:31 +0500

    deployment error resolved for module

--- amplify/functions/renderVideo/resource.ts
+++ amplify/functions/renderVideo/resource.ts
@@ -7,5 +7,5 @@ export const renderVideo = defineFunction({
   timeoutSeconds: 900,
   memoryMB: 2048,
   // Attach a Lambda Layer with ffmpeg/ffprobe binaries at /opt/bin.
-  layers: ['arn:aws:lambda:eu-north-1:242201278398:layer:ffmpeg:1'],
+  layers: ['arn:aws:lambda:eu-north-1:242201278398:layer:ffmpeg:2'],
 });
